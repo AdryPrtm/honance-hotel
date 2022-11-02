0fd9baa57c10ed916dffc155bb8fc176e96438a5
arifmuhamadiqbal <arifmi2002@gmail.com>
2022-11-02 09:03:25 +0700

    rework management files

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
+    console.log('Connected !');
     console.log('Server Running on Port:3000');
 });
 
@@ -9,8 +10,21 @@ app.get('/', (req,res) => {
     res.render('dashboard.ejs');
 });
 
-app.get('/cari', (req,res) => {
+app.get('/hasil-pencarian', (req,res) => {
     res.render('pencarian.ejs')
 })
 
+app.get('/admin', (req, res) => {
+    res.render('admin.ejs');
+})
+
+app.get('/bookings', (req, res) => {
+    res.render('bookings.ejs');
+})
+
+app.get('/rooms', (req, res) => {
+    res.render('rooms.ejs');
+})
+
+// Initiation
 app.use(express.static('Public'));
