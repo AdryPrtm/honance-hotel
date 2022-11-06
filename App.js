072595ec4b966cb3dfa2bd38da0a61c0cde22c09
Adryan Putra Pratama <adryanpratama007@gmail.com>
2022-11-06 13:10:34 +0700

    client section

--- App.js
+++ App.js
@@ -52,14 +52,15 @@ app.get("/hasil-cari/:id_room", (req,res) => {
   })
 })
 
-app.get("/checkin", (req,res) => {
-  // connection.query(`SELECT * FROM rooms WHERE id_room = '${id_room}'`, (err,rows) => {
-    // if (err) throw err;
-    // let response = JSON.parse(JSON.stringify(rows));
-    res.render("checkin.ejs");
-      // data: response,
-    // });
-  // })
+app.get("/checkin/:id_room", (req,res) => {
+  const {id_room} = req.params;
+  connection.query(`SELECT * FROM rooms WHERE id_room = '${id_room}'`, (err,rows) => {
+    if (err) throw err;
+    let response = JSON.parse(JSON.stringify(rows));
+    res.render("checkin.ejs", {
+      data: response,
+    });
+  })
 })
 
 app.get("/pembayaran", (req,res) => {
